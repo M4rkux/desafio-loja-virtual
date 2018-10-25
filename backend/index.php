044b89183f2dbba38c1a09bfd64e515c8e577d17
Marcus Amaral Rolim <m4rkux@gmail.com>
2018-10-25 09:13:27 -0400

    Ajustes layout frontend PHP sem informar os erros

--- backend/index.php
+++ backend/index.php
@@ -4,8 +4,5 @@ header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, OPTIONS');
 header("Access-Control-Allow-Headers: *");
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-
 require "controllers/RotaController.php";
 new RotaController();
